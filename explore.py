ba8bf592e1a57488ae482c0189317dbfe945c778
guedanie <danguerrero93@gmail.com>
2020-05-08 12:05:55 -0500

    new explore functions to help with viz

--- explore.py
+++ explore.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
 import model
+from scipy import stats
 
 # ------------------ #
 #    Data Prep       #
@@ -19,7 +20,38 @@ def data_distribution(df):
         f.add_subplot(5,5, i+1)
         sns.distplot(continuious.iloc[:,i], bins=5)
     plt.tight_layout()
+    plt.suptitle("How is the data generally distributed?")
     plt.show()
+
+# ------------------ #
+#        Stats       #
+# ------------------ #
+
+
+def ttail_t_test(zillow, county_1, county_2):
+    alpha = 0.05
+    subgroup_1 = zillow[zillow.county == county_1].logerror
+    subgroup_2 = zillow[zillow.county == county_2].logerror
+    
+    tstats, p = stats.ttest_ind(subgroup_1, subgroup_2)
+    
+    reject = "Because p is less than 0.05, we reject the null hypothesis"
+    fail_reject = "Because p is more than 0.05, we fail to reject the null hypothesis"
+
+    if p < alpha:
+        print(f'''
+            {reject}
+            The mean_logerror for LA = {subgroup_1.mean():.2f}
+            The mean_logerror for Orange County = {subgroup_2.mean():.2}
+            ''')
+    else:
+        print(f'''
+            {fail_reject}
+            The mean_logerror for LA = {subgroup_1.mean():.2f}
+            The mean_logerror for Orange County = {subgroup_2.mean():.2}
+            ''')
+
+
 
 # ------------------ #
 #        Viz         #
@@ -39,6 +71,7 @@ def log_error_cluster_groupings(df):
         sns.barplot(data=df, x="logerror_cluster", y=continuious_df.iloc[:,i])
         plt.title(continuious_df.columns[i])
     plt.tight_layout()
+    plt.suptitle("Do we see any groupings that have variance in logerror?")
     plt.show()
 
 def log_error_relationship_clusters(df):
@@ -49,6 +82,7 @@ def log_error_relationship_clusters(df):
         sns.barplot(data=df, x="logerror_cluster", y=features[i])
         plt.title(features[i])
     plt.tight_layout()
+    plt.suptitle("Groupings with the most variance in logerror")
     plt.show()
 
 def fips_relationships(df):
@@ -92,6 +126,7 @@ def k_cluster_relationships(df, x, features, n):
 
     for i  in range(len(features)):   
         sns.relplot(data=df, x=x, y=features[i], hue='cluster', alpha=.3)
+        plt.title("What featues have strong, or weak, relationships?")
         plt.show()
     df.drop(columns="cluster", inplace=True)
 
@@ -122,3 +157,13 @@ def compare_mse_scores(df):
     plt.title("It's hard to beat the baseline")
     plt.ylabel("MSE Score")
     plt.show
+
+def viz_location_tax_rate(zillow):
+    f, axes = plt.subplots(1, 2, figsize=(15, 9))
+    sns.scatterplot(data=zillow, y="latitude", x="longitude", hue="county", ax=axes[0])
+    sns.scatterplot(data=zillow, y="latitude", x="longitude", hue="tax_location_cluster", ax=axes[1])
+    zillow.plot.scatter(y='latitude_centroid', x='longitude_centroid', c='black', marker='x', s=1000,  label='centroid', ax=axes[1])
+    plt.suptitle("How does our clusters compare to the county's?")
+    plt.xlabel("Longitude")
+    plt.ylabel("Latitude")
+    plt.show()
